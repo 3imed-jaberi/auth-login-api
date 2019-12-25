4a658a8de217558271c479efb0e159bbb7777516
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-25 20:37:29 +0100

    better perf. 🔮 ..

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 
-const { createServer } = require('http');
 
+const { createServer } = require('http');
 const app = require('./src/app');
+
 
 const PORT = process.env.PORT ;
 
 const server = createServer(app) ; 
 
-server.listen(PORT , () => {
-  console.log(`server is runnig on port ${PORT} ...`);      
-});
+server.listen(PORT , () => console.log(`server is runnig on port ${PORT} ...`));
