4f685e06e5f1b8f475eb7aa11b0e87cefc1c9b2f
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-25 20:31:13 +0100

    passport config 🪐..

--- src/config/passport.js
+++ src/config/passport.js
@@ -4,23 +4,23 @@ const ExtractJwt= require('passport-jwt').ExtractJwt ;
 const User = require('../models/User');
 
 
+
 module.exports = (passport) => {
   let config = {};
+  
   config.secretOrKey = process.env.JWT_SECRET ;
   config.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 
-  passport.use(new JwtStrategy(config,async(jwt_payload , done)=>{
+  passport.use(new JwtStrategy(config, async (jwt_payload , done) => {
 
       try {
+          
             console.log(jwt_payload);
             
             const user = await User.findById(jwt_payload._id);
 
-            if (user){
-                return done(null , user);
-            }else{
-                return done(null,false);
-            }
+            return ( user ? done(null , user) : done(null,false) );
+
       }catch(err){
           return done(err,false);     
       }
