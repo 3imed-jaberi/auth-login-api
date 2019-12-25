4c88f80742c40f5e477bb0aa3ca04ee15fef07c9
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-25 20:33:56 +0100

    routes link 🪁..

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -1,36 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const passport = require ('passport');
+const { register, login } = require('../controllers/userController');
 
 
-const userController = require('../controllers/userController');
+
+// @route    POST api/register
+// @desc     Register user ..
+// @access   Public
+router.post('/register', register);
 
 
-// inscription ..
-router.post('/register',userController.register);
+// @route    POST api/auth
+// @desc     Authenticate user & get token ..
+// @access   Public
+router.post('/auth', login);
 
-// login with auth .. 
-router.post('/auth',userController.login);
 
+// ###################################################################### //
+router.all('*',(req, res, next) => {
+  passport.authenticate('jwt', {session:false}, (err, user) => {
 
-// Middleware for don't repeate passport.auth ...
-router.all('*',(req,res,next)=>{
-  passport.authenticate('jwt',{session:false},(err,user)=>{
     if (err || !user){
-          const error = new Error('You are not authorized to acces this area ..');
-          error.status = 401 ;
-          throw error ;
+      const error = new Error('You are not authorized to acces this area ..');
+      error.status = 401 ;
+      throw error ;
     }
+
     req.user = user;
+
     return next();
+
   })(req,res,next);
 });
+// ###################################################################### //
 
 
-// Protected Routes ...
-router.get('/profile',  // profile for exemple  .. 
-  (req,res,next)=>{
-   return res.json({message: `Hi ${req.user.name} welcome to your profile ..`});
-});
+// @route    GET api/profile
+// @desc     Simple response for test the protected route ..
+// @access   Private { Protected Route }
+router.get('/profile', (req, res, next) => res.json({ message: `Hi [${req.user.name}] welcome to your profile ..` }));
+
 
 module.exports = router ;
