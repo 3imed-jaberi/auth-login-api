dae35731a9dbb77bb4d4b1824e5f59fb922390f4
3imed-jaberi <imed_jebari@hotmail.fr>
2019-12-25 20:32:40 +0100

    cleanup structure 🪑..

--- src/models/User.js
+++ src/models/User.js
@@ -3,20 +3,35 @@ const bcrypt = require ('bcryptjs');
 
 const { Schema } = mongoose ;
 
+
+
 const UserSchema = Schema({
-  name: { type: String},
-  email: { type: String, required: true, index: true, unique: true },
-  password: { type: String, required: true},
-  joined: { type: Date, default: new Date()}
+  name: { 
+    type: String
+  },
+  email: { 
+    type: String,
+    required: true,
+    index: true,
+    unique: true
+  },
+  password: { 
+    type: String, 
+    required: true
+  },
+  joined: { 
+    type: Date, 
+    default: new Date()
+  }
 });
 
-UserSchema.pre('save',async function (next) { // problem in tis function not supprot ES6+ ..
+
+UserSchema.pre('save',async function (next) {
   
   if(!this.isModified('password')){
       return next();
   }
 
-  // Encrypt the password .. 
   try {
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(this.password,salt);
@@ -27,19 +42,17 @@ UserSchema.pre('save',async function (next) { // problem in tis function not sup
   }
 });
 
-// declarate functions can use with this model .. 
 
-UserSchema.methods.isPasswordMatch = function(password, hashed , callback ){
-  bcrypt.compare(password,hashed,(err , success)=>{
-    if (err) { return callback(err); }
-    callback(null,success);
-  });
+UserSchema.methods.isPasswordMatch = (password, hashed, callback) => {
+  bcrypt.compare(password, hashed, (err , success) => err ? callback(err) : callback(null, success));
 };
 
-UserSchema.methods.toJSON = function(){
+
+UserSchema.methods.toJSON = function() {
   const userObject = this.toObject();
   delete userObject.password;
   return userObject ;
 };
 
-module.exports = mongoose.model('User',UserSchema);
+
+module.exports = mongoose.model('User', UserSchema);
